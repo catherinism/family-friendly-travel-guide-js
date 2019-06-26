269b2e0c6ccbe8d8aab4102bc5b0040b135901cd
catherinism <catcreates13@gmail.com>
2019-06-26 19:27:26 -0400

    change container to correct class selector

--- app/assets/javascripts/guides.js
+++ app/assets/javascripts/guides.js
@@ -7,21 +7,34 @@ const guidesIndexClick = () => {
   $('.all-guides').on('click', (e) => {
     e.preventDefault()
     // console.log('hello')
+    history.pushState(null, null, 'guides')
     fetch('/guides.json')
       .then(response => response.json())
       .then(guides => {
-        $('#container').html('')
+        $('.container').html('')
         guides.forEach(guide => {
+          // console.log(guide)
           let newGuide = new Guide(guide)
-          console.log(newGuide)
+          let guideHtml = newGuide.formatIndex()
+          // console.log(guideHtml)
+          $('.container').append(guideHtml)
+          // console.log(newGuide)
         })
       })
   })
 }
 
-function Guide(id, title, destination_id) {
-  this.id = id
-  this.title = title
-  this.summary = summary
-  this.destination_id = destination_id
+function Guide(guide) {
+  this.id = guide.id
+  this.title = guide.title
+  this.summary = guide.summary
+  this.destination_id = guide.destination_id
+}
+
+Guide.prototype.formatIndex = function() {
+  console.log(this)
+  let guideHtml = `
+//     <a href="/guides/${this.id}"<h1>${this.title}</h1></a>
+  `
+  return guideHtml
 }
