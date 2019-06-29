f0954b825093df77458c94c22097f848bdd253fe
catherinism <catcreates13@gmail.com>
2019-06-29 12:47:21 -0400

    add the rest of guide attributes

--- app/serializers/guide_serializer.rb
+++ app/serializers/guide_serializer.rb
@@ -1,5 +1,5 @@
 class GuideSerializer < ActiveModel::Serializer
-  attributes :id, :title, :summary
+  attributes :id, :title, :summary, :lodging, :itinerary, :destination_location, :airport, :baby_gear_rental, :park, :zoo, :restaurant, :luggage_storage, :image
 
   belongs_to :destination
   has_many :ratings
